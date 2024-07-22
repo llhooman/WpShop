034e83bd68ccaf3a70767cc8ea1a56fc7fc8b54e
Hooman <hmn.on8@gmail.com>
2024-07-22 23:01:01 +0330

    added styles and elements

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let overlay = document.querySelector(".menu_overlay");
 let open = document.querySelector(".fa-bars");
-let close = document.querySelector(".fa-times");
+let close = document.querySelector(".fa-close");
 
 open.addEventListener("click", () => {
   overlay.style.transform = "translateY(0)";
